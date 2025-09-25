e3e8d09cfca815ba261c41d325e45f2a2248d868
@Yann-mingant-hp <upsylon.dev@gmail.com>
2025-09-25 15:12:58 +0200

    chore(init) : init project

---
+++ domains/posts/components/PostCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { Post } from "@/domains/posts/types";
+
+defineProps<{
+  post: Post;
+}>();
+</script>
+<template>
+  <div class="bg-gray-100 p-2 rounded mb-5">
+    <h2 class="text-lg font-bold">{{ post.title }}</h2>
+    <p>By {{ post.author?.name }}</p>
+    <p>{{ post.snippet }}</p>
+  </div>
+</template>
---
+++ domains/posts/server/api/posts.get.ts
@@ -0,0 +1,18 @@
+import { faker } from "@faker-js/faker";
+import type { Post } from "@/domains/posts/types";
+export default defineEventHandler(async (event): Promise<Post[]> => {
+  const users = await $fetch("/api/users");
+  const posts = [];
+  const numOfUsers = 50;
+
+  for (let index = 0; index < numOfUsers; index++) {
+    posts.push({
+      id: faker.string.uuid(),
+      title: `Post ${index + 1}`,
+      snippet: faker.lorem.paragraph(),
+      content: faker.lorem.paragraphs(),
+      author: users[index],
+    });
+  }
+  return posts;
+});
